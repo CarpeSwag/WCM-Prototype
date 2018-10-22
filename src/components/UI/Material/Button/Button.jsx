d22f3dd4e427f882f1b4dda30e6ace7b48b87696
CarpeSwag <james@garbagnati.com>
2018-10-22 03:23:30 -0700

    Cleanup MaterialButton code.

--- src/components/UI/Material/Button/Button.jsx
+++ src/components/UI/Material/Button/Button.jsx
@@ -1,8 +1,21 @@
 import Component from 'inferno-component';
 
-let timeouts = [];
+const CLICK_CAP = 100;
 
-export default class MaterialButtonContainer extends Component {
+const Button = ({children, id, label, setRef, onClick}) => (
+	<div className='material-button' id={id} onClick={onClick}
+		ref={node => setRef(node)}>
+		{children}
+		{label}
+	</div>
+);
+
+const Click = ({x, y}) => (
+	<div className='click'
+		style={{left: `${x}px`, top: `${y}px`}} />
+);
+
+export default class MaterialButton extends Component {
 	constructor(props) {
 		super(props);
 		
@@ -10,28 +23,37 @@ export default class MaterialButtonContainer extends Component {
 			clicks: []
 		}
 		
+		this.timeouts = [];
+	}
+	
+	shouldComponentUpdate(nextProps, nextState) {
+		return this.state.clicks.length != nextState.clicks.length;
 	}
 	
 	componentDidUnmount() {
-		for (let i = 0; i < timeouts.length; ++i)
-			clearTimeout(timeouts[i]);
+		for (let i = 0; i < this.timeouts.length; ++i)
+			clearTimeout(this.timeouts[i]);
 	}
 	
 	onClick(evt) {
-		let rect = this.node.getBoundingClientRect();
-		let x = evt.clientX - rect.left;
-		let y = evt.clientY - rect.top;
+		const rect = this.node.getBoundingClientRect();
+		const x = evt.clientX - rect.left;
+		const y = evt.clientY - rect.top;
 		
 		let clicks = this.state.clicks.slice();
-		clicks.push(<Click x={x} y={y} id={Math.floor(Math.random() * 10000)} />);
+		clicks.push(
+			<Click x={x} y={y} />
+		);
+		this.setState({
+			clicks: clicks
+		});
 		
-		this.setState({clicks: clicks});
-		
-		timeouts.push(setTimeout(() => {
-			timeouts = timeouts.slice(1);
-			if (timeouts.length == 0 || this.state.clicks.length > 100) {
-				// Not super efficient, but I was having issues with the DOM
-				// not actually removing old elements
+		this.timeouts.push(setTimeout(() => {
+			// Get rid of reference to timer
+			this.timeouts = this.timeouts.slice(1);
+			
+			if (this.timeouts.length == 0 || this.state.clicks.length > CLICK_CAP) {
+				// Not super efficient, but I was having issues with the DOM removing keyed elements incorrectly
 				// TODO - Look into this
 				this.setState({clicks: []});
 			}
@@ -40,25 +62,13 @@ export default class MaterialButtonContainer extends Component {
 	
 	render() {
 		return (
-			<MaterialButton
+			<Button
 				id={this.props.id}
 				label={this.props.children}
 				onClick={evt => this.onClick(evt)}
 				setRef={node => this.node = node}>
 				{this.state.clicks}
-			</MaterialButton>
+			</Button>
 		);
 	}
 }
-
-const MaterialButton = ({children, id, label, setRef, onClick}) => (
-	<div className='material-button' id={id} onClick={onClick}
-		ref={node => setRef(node)}>
-		{children}
-		{label}
-	</div>
-);
-
-const Click = ({id, x, y}) => (
-	<div id={id} className='click' style={{left: x, top: y}} />
-);
